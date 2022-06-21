d32849430744b3d007f5df45cca0b5b5d252da42
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-06-21 10:41:10 -0400

    Separate todo-list into its own component

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,16 @@
-import { Todo } from './interfaces/todo';
+import { TodoList } from "components/todo-list";
 
 export class App {
-  todos: Todo[] = [];
   todoDescription = '';
+  todoList : TodoList;
 
   addTodo() {
     if (this.todoDescription) {
-      this.addOne(this.todoDescription);
+      this.todoList.addOne(this.todoDescription);
       this.todoDescription = '';
     }
   }
 
-  private addOne(todoDescription: string) {
-    this.todos.push({
-      description: todoDescription,
-      done: false
-    });
-  }
-
-  removeTodo(todo: Todo) {
-    let index = this.todos.indexOf(todo);
-    if (index !== -1) {
-      this.todos.splice(index, 1);
-    }
-  }
 }
+
+
---
+++ src/components/todo-list.ts
@@ -0,0 +1,22 @@
+import { Todo } from "interfaces/todo";
+
+export class TodoList {
+  todos: Todo[] = [
+    { description: "Make bed", done: true, },
+    { description: "Take out trash", done: false }
+  ];
+
+  addOne(todoDescription: string) {
+    this.todos.push({
+      description: todoDescription,
+      done: false
+    });
+  }
+
+  removeTodo(todo: Todo) {
+    let index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
